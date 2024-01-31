2d8c5a3917eccce5cd9a226e781478c811799556
DaianeMGouvea <daianegouvea.dev@gmail.com>
2024-01-31 15:56:43 -0300

    authentication user in self_service

---
+++ fe_lab_clinicas_self_service/lib/src/pages/splash_page/splash_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Scaffold(
+        body: Center(
+          child: Image.asset('assets/images/logo_vertical.png'),
+        ),
+      ),
+    );
+  }
+}
