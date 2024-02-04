cfc9a1ddc8ddb2c49ead34f6998be0fc44bd24d0
DaianeMGouvea <daianegouvea.dev@gmail.com>
2024-02-03 22:34:48 -0300

    create page done

--- fe_lab_clinicas_self_service/lib/src/modules/self_service/done/done_page.dart
+++ fe_lab_clinicas_self_service/lib/src/modules/self_service/done/done_page.dart
@@ -28,10 +28,83 @@ class DonePage extends StatelessWidget {
                 Image.asset('assets/images/stroke_check.png'),
                 const SizedBox(height: 15),
                 const Text(
-                  'CONFIRA A SUA FOTO',
+                  'Sua senha é',
                   style: LabClinicasTheme.titleSmallStyle,
                 ),
-                const SizedBox(height: 20),
+                const SizedBox(height: 24),
+                Container(
+                  constraints:
+                      const BoxConstraints(minWidth: 218, minHeight: 48),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                  decoration: BoxDecoration(
+                    color: LabClinicasTheme.orangeColor,
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: const Text(
+                    'BG4232',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 40),
+                const Text.rich(
+                  TextSpan(
+                    style: TextStyle(
+                        color: LabClinicasTheme.blueColor,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold),
+                    children: [
+                      TextSpan(text: 'AGUARDE!\n'),
+                      TextSpan(text: 'Sua senha será chamada no painel'),
+                    ],
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 40),
+                Row(
+                  children: [
+                    Expanded(
+                      child: SizedBox(
+                        height: 48,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text('IMPRIMIR SENHA'),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 16),
+                    Expanded(
+                      child: SizedBox(
+                        height: 48,
+                        child: OutlinedButton(
+                          onPressed: () {},
+                          child: const Text('ENVIAR SENHA VIA SMS'),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                SizedBox(
+                  height: 48,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: LabClinicasTheme.orangeColor,
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      'FINALIZAR',
+                      style:
+                          TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
